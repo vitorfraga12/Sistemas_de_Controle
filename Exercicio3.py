77cfb9275c7de74c5606f9e7630bba2e73390924
abrxao <abrxao@gmail.com>
2024-04-23 10:10:20 -0300

    Resolvendo o item 4

--- Exercicio3.py
+++ Exercicio3.py
@@ -1,20 +1,32 @@
 from scipy import signal
 import matplotlib.pyplot as plt
-import numpy as np
-#ITEM 3
-#Para encontrar a resposta a u(t) =  e^(−3t)*δ(t)
-#Sabemos que G(s) = -10/(-5*s**2 -10*s - 25)
+from sympy import symbols, inverse_laplace_transform, exp
+
+# Definindo as variáveis "t" no domínio do Tempo e "s" no domínio de Laplace
+s, t = symbols('s t')
+
+# Definindo as funções que eu quero encontrar a transformada inversa de Laplace
+F1 = (4*s**2 + 15*s+ 19)/(s**3 + 2*s**2 + s )
+
+# Encontrando as transformada inversas de Laplace
+f1 = inverse_laplace_transform(F1, s, t)
+
+# Imprimir o resultado
+print(f'Temos que as transformadas inversas de Laplace são:\n f1(t) = {f1}')
+##############################################################
+
 num = [4,15,19]
-den = [1,2,1,0]
+den = [1,2,1]
 sys = signal.TransferFunction(num, den)
 t, y = signal.impulse(sys)
-y_respot = y * 1
+
+print('Resposta ao impulso y(t) = ', sys.inputs)
 
 plt.figure()
-plt.plot(t, y_respot, label='y(t)')
+plt.plot(t, y, label='y(t)')
 plt.xlabel('Tempo (s)')
 plt.ylabel('y(t)')
-plt.title('Saída y(t) em resposta a u(t) = (t)')
+plt.title('Saída y(t) em resposta a u(t)')
 plt.legend()
 plt.grid(True)
 plt.show()
